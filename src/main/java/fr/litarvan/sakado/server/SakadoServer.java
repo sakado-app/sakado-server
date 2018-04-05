8b3e4fb97764f5c587bab2c05d7e58490f348a7a
Adrien Navratil <adrien1975@live.fr>
2018-04-05 17:09:11 +0200

    Version increment

--- src/main/java/fr/litarvan/sakado/server/SakadoServer.java
+++ src/main/java/fr/litarvan/sakado/server/SakadoServer.java
@@ -47,7 +47,7 @@ import java.net.InetSocketAddress;
 
 public class SakadoServer implements App
 {
-    public static final String VERSION = "1.0.0-BETA";
+    public static final String VERSION = "1.1.0-BETA";
 
     private static final Logger log = LogManager.getLogger("SakadoServer");
 
