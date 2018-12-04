948b6cf04e3737aa6ac597482f01abbca9464246
Adrien Navratil <adrien1975@live.fr>
2018-12-04 17:38:27 +0100

    Deleted TODO

--- src/main/java/fr/litarvan/sakado/server/SakadoServer.java
+++ src/main/java/fr/litarvan/sakado/server/SakadoServer.java
@@ -46,8 +46,6 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 
-// TODO: Check reminders permission
-
 public class SakadoServer implements App
 {
     public static final String VERSION = "1.5.0-BETA";
